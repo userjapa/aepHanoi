e39363ea8210ad53ca8e7a969531c05c5f72d2cd
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-03-27 18:45:19 -0300

    Pronto

--- src/test/java/com/mycompany/aep4/HanoiTeste.java
+++ src/test/java/com/mycompany/aep4/HanoiTeste.java
@@ -10,55 +10,57 @@ public class HanoiTeste {
     @Test
     public void quantPinos() {
         Hanoi hanoi = new Hanoi();
-        hanoi.iniciarDiscos(6);
-        assertThat(hanoi.pino1.toString()).isEqualTo("[6, 5, 4, 3, 2, 1]");
+        hanoi.iniciarDiscos(8);
+        assertThat(hanoi.pino1.toString()).isEqualTo("[8, 7, 6, 5, 4, 3, 2, 1]");
     }
     
     @Test
     public void discosEmpilhados() {
         Hanoi hanoi = new Hanoi();
-        hanoi.iniciarDiscos(5);
-        assertThat(hanoi.discos.toString()).isEqualTo("[5, 4, 3, 2, 1]");
+        hanoi.iniciarDiscos(7);
+        assertThat(hanoi.discos.toString()).isEqualTo("[7, 6, 5, 4, 3, 2, 1]");
     }
     
     @Test
     public void diametroDiscos() {
         Hanoi hanoi = new Hanoi();
-        hanoi.iniciarDiscos(4);
+        hanoi.iniciarDiscos(6);
         String diametros = " - ";
         for (int i = 0; i < hanoi.discos.size(); i++) {
             diametros += "DISCO "+(i+1)+": DIAMETRO "+(i+1)+" - \n";
         }
-        assertThat(diametros).isEqualTo(" - DISCO 1: DIAMETRO 1 - \nDISCO 2: DIAMETRO 2 - \nDISCO 3: DIAMETRO 3 - \nDISCO 4: DIAMETRO 4 - \n");
+        assertThat(diametros).isEqualTo(" - DISCO 1: DIAMETRO 1 - \nDISCO 2: DIAMETRO 2 - \nDISCO 3: DIAMETRO 3 - \nDISCO 4: DIAMETRO 4 - \nDISCO 5: DIAMETRO 5 - \nDISCO 6: DIAMETRO 6 - \n");
     }
     
     @Test
     public void tranferencia() {
         Hanoi hanoi = new Hanoi();
-        hanoi.iniciarDiscos(7);
+        hanoi.iniciarDiscos(5);
         assertThat(hanoi.validaMov(hanoi.pino1, hanoi.pino1)).isEqualTo(false);
     }
     
     @Test
     public void movInvalido() {
         Hanoi hanoi = new Hanoi();
-        hanoi.iniciarDiscos(8);
+        hanoi.iniciarDiscos(4);
         hanoi.mov1to2();
         hanoi.mov1to3();
         hanoi.mov2to3();
         hanoi.mov1to2();
         hanoi.mov1to2();
         
-        assertThat(hanoi.pino1.toString()).isEqualTo("[8, 7, 6, 5, 4]");
+        assertThat(hanoi.pino1.toString()).isEqualTo("[4]");
         assertThat(hanoi.pino3.toString()).isEqualTo("[2, 1]");
     }
     
     @Test
     public void grafico() {
         Hanoi hanoi = new Hanoi();
-        hanoi.iniciarDiscos(4);
-        
-        assertThat(hanoi.mov1to2()).isEqualTo(true);
+        hanoi.iniciarDiscos(3);
+        hanoi.mov1to2();
+        assertThat(hanoi.graf(hanoi.pino1)).isEqualTo("\n\n|++|\n|+++|\n====================\n\n");
+        assertThat(hanoi.graf(hanoi.pino2)).isEqualTo("\n\n|+|\n====================\n\n");
+        assertThat(hanoi.graf(hanoi.pino3)).isEqualTo("\n\n====================\n\n");
     }
     
 }
